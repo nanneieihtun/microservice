e72728aad29c4cce1326269b599c43610e6e971f
Nann Ei Ei Htun <nanneieihtun@gmail.com>
2023-04-03 20:30:17 +0630

    Add files via upload

---
+++ WebService.Interface.Service.Test/Program.cs
@@ -0,0 +1,34 @@
+﻿using Castle.Windsor;
+using System.ServiceModel;
+
+namespace WebService.Interface.Service.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var c = BuildContainer())
+            {
+            }
+        }
+
+        private static IWindsorContainer BuildContainer()
+        {
+         //  var applicationServiceBase = AppConfig.GetAppSetting<string>("ApplicationServiceBase");
+
+            var container = new WindsorContainer();
+
+          //  container.AddFacility<WcfFacility>();
+
+            var binding = new NetTcpBinding(SecurityMode.None)
+            {
+                PortSharingEnabled = true,
+                MaxBufferPoolSize = 2147483647,
+                MaxBufferSize = 2147483647,
+                MaxReceivedMessageSize = 2147483647
+            };
+
+            return container;
+        }
+    }
+}
